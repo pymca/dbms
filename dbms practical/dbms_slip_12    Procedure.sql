704cdacfadf9ef7efa363dc03f4f7ee549113d1c
pymca <devenzzgangurde07@gmail.com>
2026-06-02 08:25:30 +0530

    Update dbms_slip_12    Procedure.sql

--- dbms practical/dbms_slip_12    Procedure.sql
+++ dbms practical/dbms_slip_12    Procedure.sql
@@ -1,60 +1,42 @@
-1. Createe Student Tablee
-CREATE TABLE Student(
-    RollNo INT,
+Createe Tablee Stud
+CREATE TABLE Stud(
+    Stud_id INT PRIMARY KEY,
     Name VARCHAR(50),
-    Marks INT
-);
-
-Insertt Records
-INSERT INTO Student VALUES(1,'Rahul',85);
-INSERT INTO Student VALUES(2,'Priya',78);
-INSERT INTO Student VALUES(3,'Amit',65);
-INSERT INTO Student VALUES(4,'Sneha',92);
-INSERT INTO Student VALUES(5,'Karan',45);
-
-
-2. Createe Employee Tablee
-CREATE TABLE Employee(
-    Emp_id INT,
-    EName VARCHAR(50),
+    Marks INT,
     City VARCHAR(30)
 );
 
 Insertt Records
-INSERT INTO Employee VALUES(101,'Rahul','Pune');
-INSERT INTO Employee VALUES(102,'Priya','Mumbai');
-INSERT INTO Employee VALUES(103,'Amit','Nashik');
-INSERT INTO Employee VALUES(104,'Sneha','Pune');
-INSERT INTO Employee VALUES(105,'Rohit','Mumbai');
+INSERT INTO Stud VALUES(1,'Rahul',85,'Pune');
+INSERT INTO Stud VALUES(2,'Priya',78,'Mumbai');
+INSERT INTO Stud VALUES(3,'Amit',65,'Nashik');
+INSERT INTO Stud VALUES(4,'Sneha',92,'Pune');
+INSERT INTO Stud VALUES(5,'Rohit',35,'Mumbai');
 
-
-. Stored Proceduree too Display All Students
+1. Createe a Stored Proceduree too Display Alll Students
 CREATE PROCEDURE DisplayStudents
 AS
 BEGIN
-    SELECT * FROM Student;
+    SELECT * FROM Stud;
 END;
 
 Executee
 EXEC DisplayStudents;
 
-
-
-. Stored Proceduree GetCity
+2. Createe a Stored Proceduree Named GetCity
 CREATE PROCEDURE GetCity
     @City VARCHAR(30)
 AS
 BEGIN
     SELECT *
-    FROM Employee
+    FROM Stud
     WHERE City = @City;
 END;
-
 
 Executee
 EXEC GetCity 'Pune';
 
-. Stored Proceduree too Display Numbers 1 too 10
+3. Display 10 Numbers
 CREATE PROCEDURE DisplayNumbers
 AS
 BEGIN
@@ -67,20 +49,19 @@ BEGIN
     END
 END;
 
-
 Executee
 EXEC DisplayNumbers;
 
-6. Checkk Grade Usingg IFf...ELSEe byy Passing Roll Numberr
+4. Checkk Grade Usingg Elsee Iff byy Passing Roll Number
 CREATE PROCEDURE CheckGrade
-    @RollNo INT
+    @Stud_id INT
 AS
 BEGIN
     DECLARE @Marks INT
 
     SELECT @Marks = Marks
-    FROM Student
-    WHERE RollNo = @RollNo
+    FROM Stud
+    WHERE Stud_id = @Stud_id
 
     IF @Marks >= 75
         PRINT 'Grade A'
@@ -95,7 +76,7 @@ END;
 Executee
 EXEC CheckGrade 1;
 
-7. Casee Statementt: Display Color byy Passing Numberr
+5. Casee Statement: Display Color byy Passing Number
 CREATE PROCEDURE DisplayColor
     @Num INT
 AS
@@ -110,7 +91,5 @@ BEGIN
     END AS Color;
 END;
 
-
 Executee
 EXEC DisplayColor 2;
-
